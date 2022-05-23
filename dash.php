ca2005e0d8b91a19852a41010965cb6842746799
unknown <jamesofficial31@gmail.com>
2022-05-24 01:14:13 +0200

    dashboard

--- dash.php
+++ dash.php
@@ -1,39 +1,60 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+
+        <title>ASSIGNMENT</title>
+    </head>
+    <body>
+        <header class="text-white p-1 text-center" style="background-color:red;">
+            <h5>Assignment</h5>
+        </header>
     <link rel="stylesheet" href="index.css">
-    <title>Assignment</title>
-</head>
-<body>
     <div class="container">
         <div class="header">
-            <h2>Assignment</h2>
             <ul>
                 <a href="php/logout.php"><li>Logout</li></a>
             </ul>
         </div>
-        <div class="dashContent">
-            <div class="left">
-                <img src="icons/2.jpg" style="width:100%;height: 60%">
-            </div>
-            <div class="right">
-                <ul>
-                    <li><img src="icons/key.png"> Edit Profile</li>
-                    <li><img src="icons/key.png"> Change Password</li>
-                    <li><img src="icons/key.png"> SignIn activities</li>
-                    <li><img src="icons/key.png"> Session timeout</li>
-                    <li><img src="icons/key.png"> Memorisation</li>
-                    <li><img src="icons/key.png"> Job Feed content</li>
-                    <li><img src="icons/key.png"> Email & SMS notification</li>
-                    <li><img src="icons/key.png"> Push Notification</li>
-                    <li><img src="icons/key.png"> Delete my account</li>
-
-                </ul>
+        <div class="settings">
+        <!-- header-setting -->
+            <div style="top: 3px;" class="shadow-lg mx-3 w-100" style="width: 280px; top:100px;">
+                <div class="d-flex justify-content-center align-items-center">
+                <p class="ps-4 mt-2 text-primary fw-bold shadow set">ACCOUNT SETTING</p>
+                </div>
+                <div class="menus card ps-4">
+                    <a href=""><i class="fa fa-pencil"></i> Edit Profile</a>
+                </div>
+                <div class="menus card ps-4">
+                    <a href=""><i class="fas fa-key"></i> Change Password</a>
+                </div>
+                <div class="menus card ps-4">
+                    <a href=""><i class="fa fa-flash"></i> Sign in Activities</a>
+                </div>
+                <div class="menus card ps-4">
+                    <a href=""><i class="fa fa-clock-o"></i> Session Timeout</a>
+                </div>
+                <div class="menus card ps-4">
+                    <a href=""><i class="fa fa-heartbeat"></i> Memorialisation</a>
+                </div>
+                <div class="menus card ps-4">
+                    <a href=""><i class="fa fa-tag"></i> Jobs Feed Content</a>
+                </div>
+                <div class="menus card ps-4">
+                    <a href=""><i class="fa fa-envelope"></i> Email & SMS Notification</a>
+                </div>
+                <div class="menus card ps-4">
+                    <a href=""><i class=" fa fa-bell"></i> Push Notification</a>
+                </div>
+                <div class="menus card ps-4">
+                    <a href=""><i class="fa fa-trash"></i> Delete my Account</a>
+                </div>
             </div>
         </div>
-</div>
 </body>
 </html>
